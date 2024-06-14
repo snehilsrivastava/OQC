692246c7ec7b808a7df9e72a4656f9eff617efa5
snehilsr <snehil.s@indkaltechno.onmicrosoft.com>
2024-06-14 12:13:45 +0530

    fix gitignore

--- oqc_model/oqc/authapp/models.py
+++ oqc_model/oqc/authapp/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Employee(models.Model):
-       user_type = models.CharField(max_length=50)
-       username  = models.CharField(max_length=255)
-       password =  models.CharField(max_length=255)
-       last_login = models.TimeField(max_length=20)
-       def __str__(self):
-              return f"{self.username} "
+ user_type = models.CharField(max_length=50)
+ username  = models.CharField(max_length=255)
+ password =  models.CharField(max_length=255)
+ last_login = models.TimeField(auto_now_add=True)
+ def __str__(self):
+        return f"{self.username} "
