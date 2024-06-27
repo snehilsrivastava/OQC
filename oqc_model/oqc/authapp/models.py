4aa96e2412cdef042cf1ef8d357dff9f4a3fae54
unhappyTWO <akshay.v@indkaltechno.onmicrosoft.com>
2024-06-27 17:33:20 +0530

    OTP done

--- oqc_model/oqc/authapp/models.py
+++ oqc_model/oqc/authapp/models.py
@@ -3,9 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Employee(models.Model):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
     user_type = models.CharField(max_length=50)
     username  = models.CharField(max_length=255)
     password =  models.CharField(max_length=255)
     last_login = models.TimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.username} "
+
+
+class OTP(models.Model):
+    user = models.CharField(max_length=255)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
