bf78f9e223f408af3300d201476fa36ed9382d25
snehilsr <snehil.s@indkaltechno.onmicrosoft.com>
2024-07-11 10:43:26 +0530

    added forgot password page

--- oqc_model/oqc/authapp/urls.py
+++ oqc_model/oqc/authapp/urls.py
@@ -4,15 +4,17 @@ from django.urls import path	 # URL routing
 from .views import * # Import views 
 from django.conf import settings # Application settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns # Static files serving
-#from . import views
+from . import views
 
 # Define URL patterns
 urlpatterns = [
-	path('home/', home, name="recipes"),	 # Home page	
-	path('login/', login_page, name='login_page'), # Login page 
-    path('register/', register_page, name='register'),# Registration page
-  
-  
+	path('login/', views.login_page, name='login_page'),
+    path('register/', views.register_page, name='register'),
+	path('register/send_otp/', views.send_otp, name='send_otp'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('forgot_password/send_otp/', views.forgot_password_send_otp, name='forgot_password_send_otp'),
+    path('forgot_password/verify_otp/', views.forgot_password_verify_otp, name='forgot_password_verify_otp'),
+    path('forgot_password/update_password/', views.forgot_password_update, name='forgot_password_update'),	
 ]
 
 # Serve media files if DEBUG is True (development mode)
--- oqc_model/oqc/authapp/views.py
+++ oqc_model/oqc/authapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
@@ -18,10 +18,6 @@ def authenticate(username=None, password=None):
     if check_password(password, login_user.password):
         return login_user
     return None
-
-# Define a view function for the home page
-def home(request):
-    return render(request, 'home.html')
 
 # Define a view function for the login page
 def login_page(request):
@@ -81,78 +77,130 @@ def verify_otp(user, otp_code):
             return 3
 
 # send OTP button
-def send_otp(request, new_employee):
+def send_otp(request):
     if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
         # Check if a user with the provided username already exists
-        if Employee.objects.filter(username=new_employee.username).exists():
-            messages.warning(request, "Email is already registered.")
-            return redirect('/au/register/')
-        
-        # delete_expired_otps()
+        if Employee.objects.filter(username=username).exists():
+            messages.warning(request, "Username already exists")
+            return JsonResponse({'redirect_url': '/au/login/'})
+        # Check if the email address is valid
+        valid_domains = ['indkal.com', 'indkaltechno.onmicrosoft.com']
+        if username.split('@')[-1] not in valid_domains:
+            return JsonResponse({'warning': True, 'message': 'Please enter a valid email address'})
+        # Check if the password is valid
+        validity = validate_password(password)
+        if validity:
+            return JsonResponse({'warning': True, 'message': validity})
+
         otp = generate_otp()
-        messages.info(request, f"{otp}")
-        OTP.objects.create(user=new_employee.username, otp=otp, created_at=datetime.now(), is_verified=False)
+        OTP.objects.create(user=username, otp=otp, created_at=datetime.now(), is_verified=False)
         subject = 'OTP'
-        message = f'Hi {new_employee.first_name},\nYour OTP is {otp}. It expires in 5 minutes.'
+        message = f'Hi {first_name} {last_name},\nYour OTP is {otp}. It expires in 5 minutes.'
         email_from = settings.EMAIL_HOST_USER
-        recipient_list = [new_employee.username]
+        recipient_list = [username]
         send_mail(subject, message, email_from, recipient_list)
-        messages.success(request, "OTP sent successfully.")
-    return
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
 
 def validate_password(password):
-    flag = 0
-    while True:
-        if (len(password)<8):
-            return ("Passwords must be at least 8 characters")
-        elif not re.search("[a-z]", password):
-            return ("Passwords must have at least a lowercase letter")
-        elif not re.search("[A-Z]", password):
-             return ("Passwords must have at least an uppercase letter")
-        elif not re.search("[0-9]", password):
-            return ("Passwords must have at least a digit")
-        # elif not re.search("[_#@$]" , password):
-            # return ("Password must have a special symbol (_, @, #, $)")
-        elif re.search("\s" , password):
-            return ("Password must not have any whitespace characters")
-        else:
-            return None
+    if (len(password)<8):
+        return ("Passwords must be at least 8 characters")
+    elif not re.search("[a-z]", password):
+        return ("Passwords must have at least a lowercase letter")
+    elif not re.search("[A-Z]", password):
+        return ("Passwords must have at least an uppercase letter")
+    elif not re.search("[0-9]", password):
+        return ("Passwords must have at least a digit")
+    # elif not re.search("[_#@$]" , password):
+        # return ("Password must have a special symbol (_, @, #, $)")
+    elif re.search("\s" , password):
+        return ("Password must not have any whitespace characters")
+    else:
+        return None
         
-
 # sign up button
-# Define a view function for the registration page
 def register_page(request):
     if request.method == 'POST':
-        act = request.POST.get('action')
+        # act = request.POST.get('action')
         username = request.POST.get('username')
         fname = request.POST.get('first_name')
         lname = request.POST.get('last_name')
         pword = request.POST.get('password')
-        if username.split('@')[-1] != "indkal.com":
-             messages.warning(request, "Please enter a valid email address.")
-             return render(request, 'register.html')
-        validity = validate_password(pword)
-        if validity:
-             messages.warning(request, f"{validity}")
-             return render(request, 'register.html')
-        new_employee = Employee(username=username, first_name=fname, last_name=lname, password=pword)
+        pword = make_password(pword)
         in_otp = request.POST.get('OTP')
-        if act=='send_otp':
-            send_otp(request, new_employee)
-            return render(request, 'register.html', context={"employee":new_employee, "password":pword})
-        elif act=='sign_up':
-            msg = verify_otp(username, in_otp)
-            print(msg)
-            match (msg):
-                case 1:
-                    new_employee.save()
-                    messages.success(request, "OTP Verified successfully.")
-                    messages.success(request, "Account created!")
-                    return redirect('/au/login/')
-                case 2:
-                    messages.error(request, "Invalid or Expired OTP.")
-                case 3:
-                    messages.error(request, "Invalid OTP.")
-        elif act=="otp_resend":
-            new_employee = send_otp()
+        new_employee = Employee(username=username, first_name=fname, last_name=lname, password=pword)
+        # delete_expired_otps()
+        msg = verify_otp(username, in_otp)
+        match (msg):
+            case 1:
+                new_employee.save()
+                messages.success(request, "Account created!")
+                return redirect('/au/login/')
+            case 2:
+                messages.error(request, "Invalid or Expired OTP.")
+            case 3:
+                messages.error(request, "Invalid OTP.")
     return render(request, 'register.html')
+
+def forgot_password(request):
+    return render(request, 'forgot_password.html')
+
+def forgot_password_send_otp(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        # Check if a user with the provided username already exists
+        if not Employee.objects.filter(username=username).exists():
+            return JsonResponse({'error': True, 'message': 'Email doesn\'t exist'})
+        employee = Employee.objects.get(username=username)
+        first_name = employee.first_name
+        last_name = employee.last_name
+        otp = generate_otp()
+        OTP.objects.create(user=username, otp=otp, created_at=datetime.now(), is_verified=False)
+        subject = 'OTP'
+        message = f'Hi {first_name} {last_name},\nYour OTP is {otp}. It expires in 5 minutes.'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [username]
+        # send_mail(subject, message, email_from, recipient_list)
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
+
+def forgot_password_verify_otp(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        in_otp = request.POST.get('OTP')
+        # delete_expired_otps()
+        msg = verify_otp(username, in_otp)
+        print(in_otp)
+        print(username)
+        print(msg)
+        match (msg):
+            case 1:
+                return JsonResponse({'success': True})
+            case 2:
+                return JsonResponse({'error': True, 'message': 'Invalid or Expired OTP.'})
+            case 3:
+                return JsonResponse({'error': True, 'message': 'Invalid OTP.'})
+    return JsonResponse({'success': False, 'message': 'Invalid request!!!'}, status=400)
+
+def forgot_password_update(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm-password')
+        validity = validate_password(password)
+        if validity:
+            print(validity)
+            return JsonResponse({'error': True, 'message': validity})
+        if password != confirm_password:
+            return JsonResponse({'error': True, 'message': 'Passwords do not match'})
+        employee = Employee.objects.get(username=username)
+        employee.password = make_password(password)
+        employee.save()
+        messages.success(request, "Password changed successfully!")
+        return JsonResponse({'redirect_url': '/au/login/'})
+    return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
